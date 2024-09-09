ea634bf668ec996ff3e1c5bc71c97c9c2b98d5bc
cyberbeam524 <122214743+cyberbeam524@users.noreply.github.com>
2024-09-09 23:23:21 +0800

    added adsense config

--- nextjs-web-scraper3/pages/index.js
+++ nextjs-web-scraper3/pages/index.js
@@ -307,7 +307,7 @@ const Home = () => {
 
   return (
     <div>
-      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9744548568508745" crossOrigin="anonymous"></Script>
+      {/* <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9744548568508745" crossOrigin="anonymous"></Script> */}
       <Navbar />
       <HeroSection />
 
